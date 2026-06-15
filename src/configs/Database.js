ac5e2b4700e9a17c6799994eedf1e8b13395cd75
MIGUEL VALLIM BARBIERI <miguel.barbieri3@senaisp.edu.br>
2026-06-15 08:30:51 -0300

    meu projeto pbe2

---
+++ src/configs/Database.js
@@ -0,0 +1,38 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+// Singleton para a conexão com o banco de dados
+class Database {
+    static #instance = null;
+    #pool = null;
+
+    #createPool() {
+        this.#pool = mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT,
+            waitForConnections: true,
+            connectionLimit: 100,
+            queueLimit: 0,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+    }
+
+    static getInstance() {
+        if (!Database.#instance) {
+            Database.#instance = new Database();
+            Database.#instance.#createPool();
+        }
+        return Database.#instance;
+    }
+
+    getPool() {
+        return this.#pool;
+    }
+}
+
+export const connection = Database.getInstance().getPool();
